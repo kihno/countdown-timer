6544fdcf3cbaecaca759b7a25136313104611c30
kihno <stistyle13@gmail.com>
2023-12-13 16:30:49 -0800

    Implement functions to start, stop and reset the countdown timer

--- src/countdown-timer.tsx
+++ src/countdown-timer.tsx
@@ -1,41 +1,70 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const CountdownTimer = () => {
-  // new Date(year, monthIndex, day, hours, minutes)
+  // New Date(year, monthIndex, day, hours, minutes)
   const fiveMinutes = new Date(0, 0, 0, 0, 5);
   const [timeRemaining, setTimeRemaining] = useState(fiveMinutes);
+  const intervalId = useRef<number | null>(null);
+
+  // Clear the interval on unmount
+  useEffect(() => {
+    return () => {
+      if (intervalId.current !== null) {
+        clearInterval(intervalId.current);
+      }
+    }
+  }, []);
 
   const formatDate = (dateObject) => {
-    // get minutes and seconds
+    // Get minutes and seconds
     const minutes = dateObject.getMinutes();
     const seconds = dateObject.getSeconds();
 
-    // padd seconds with preceding zero
+    // Pad seconds with preceding zero
     const paddedSeconds = seconds.toString().padStart(2, "0");
 
-    // return formatted ate
+    // Return formatted ate
     return `${minutes}:${paddedSeconds}`;
   }
 
-  // store interval id as ref
-  const intervalId = useRef<number | null>(null);
-
   const handleClickStart = () => {
-    // update current value of the ref
-    // does not trigger a rerender
+    // If a timer is already running, no not start another one
+    if (intervalId.current !== null) {
+      return;
+    }
+
     intervalId.current = window.setInterval(() => {
-      // ...
+      setTimeRemaining(
+        (prevTimeRemaining) => new Date(prevTimeRemaining.getTime() - 1000)
+      )
     }, 1000);
   };
 
+  const handleClickStop = () => {
+    // If timer is already stopped, do nothing
+    if (intervalId.current === null) {
+      return;
+    }
+
+    clearInterval(intervalId.current);
+    // Clear current value of the ref, does not trigger rerender
+    intervalId.current = null;
+  }
+
+  const handleReset = () => {
+    if (intervalId.current !== null) {
+      clearInterval(intervalId.current);
+    }
+    setTimeRemaining(fiveMinutes);
+  }
 
   return(
     <div>
       <h1>{formatDate(timeRemaining)}</h1>
       <div>
-        <button>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        <button onClick={handleClickStart}>Start</button>
+        <button onClick={handleClickStop}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
     </div>
   )
